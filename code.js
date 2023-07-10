a95910b1866f7472cbc9fca040e198f13a302049
Amy Belcher <amylei7@hotmail.com>
2023-07-10 16:13:37 -0400

    getting started with the function layout for the game

--- code.js
+++ code.js
@@ -1,2 +1,27 @@
-
 // Your Code Here.
+let gameArray= [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+];
+
+let currentPlayer;
+
+function moveLeft() {}
+function moveRight() {}
+function moveUp() {}
+function moveDown() {}
+
+function addRandomTile() {}
+
+function checkIfGameHasEnded() {
+    return false
+}
+
+function resetBoard() {}
+//deletes old DOM and renders new DOM 
+
+function renderBoard(){}
