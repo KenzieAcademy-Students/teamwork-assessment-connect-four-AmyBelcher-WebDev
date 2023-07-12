41ea005ee56f49669a14c88c75731ffd008f945d
alexis gray <alexisjgray@yahoo.com>
2023-07-11 17:54:53 -0700

    placed start button

--- code.js
+++ code.js
@@ -6,51 +6,57 @@ let board = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
-]
+];
 
-let currentPlayer = 1
-let numberOfDiscsPlayed = 0
+let currentPlayer = 1;
+let numberOfDiscsPlayed = 0;
 
 let dropDisc = function (event) {
   //disc dropping code
-  checkForGameOver(board)
-}
+  checkForGameOver(board);
+};
 
 let whoIsTheWinner = function (boardModel) {
   //check for 4 in a row
   //return the player number who won, or null
-}
+};
 
 let isGameATie = function (boardModel) {
   //returns a boolean
-}
+};
 
-let nextPlayer = function () {}
+let nextPlayer = function () {};
 
 let checkForGameOver = function (boardModel) {
   //look at the board variable to find 4-in a row
-  let winner = whoIsTheWinner(boardModel)
-  let tieGame = isGameATie(boardModel)
+  let winner = whoIsTheWinner(boardModel);
+  let tieGame = isGameATie(boardModel);
   if (winner !== null || tieGame) {
-    gameOver(winner, tieGame)
+    gameOver(winner, tieGame);
   } else {
-    nextPlayer()
+    nextPlayer();
   }
-}
+};
 
 let displayMessage = function (message) {
   //Show message in the message box on the DOM
-}
+};
 
-let setUpClickHandlers = function () {}
+let setUpClickHandlers = function () {};
 
 let initializeGame = function () {
   //initialize various variables
-  setUpClickHandlers()
-}
+  setUpClickHandlers();
+};
 
-initializeGame()
-  
+initializeGame();
+
+let startButton = document.createElement("button");
+startButton.innerHTML = "Start";
+document.body.append(startButton);
+function start() {
+  alert("`${currentPlayer}` turn");
+}
 
 let testBoard1 = [
   [0, 0, 0, 0, 0, 0, 0],
@@ -59,7 +65,7 @@ let testBoard1 = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 1, 1, 1, 1, 0, 0],
-]
+];
 
 let testBoard2 = [
   [0, 0, 0, 0, 0, 0, 0],
@@ -68,7 +74,7 @@ let testBoard2 = [
   [0, 0, 0, 2, 0, 0, 0],
   [0, 0, 0, 2, 0, 0, 0],
   [0, 1, 1, 2, 1, 0, 0],
-]
+];
 
 let testBoard3 = [
   [0, 0, 0, 0, 0, 0, 0],
@@ -77,4 +83,4 @@ let testBoard3 = [
   [2, 0, 0, 0, 0, 0, 0],
   [2, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
-]
+];
