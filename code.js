e6d06d5ada43b395be3415656fdb38a7bda8b131
Amy Belcher <amylei7@hotmail.com>
2023-07-13 17:58:58 -0400

    completed dropDisc function and fixed html

--- code.js
+++ code.js
@@ -35,8 +35,8 @@ let numberOfDiscsPlayed = 0
 //   checkForGameOver(board)
 // }
 
-let columnFull = function (columnArray) {
-  if (columnArray.includes(0) === true) {
+let columnFull = function (boardModel, columnIndex) {
+  if (boardModel[0][columnIndex] === 0) {
     return false
   } else {
     return true
@@ -47,45 +47,43 @@ let gameDiv = document.querySelector(".game");
 let tableGrid = document.querySelector(".game.div");
 
 
-let dropDisc = function (columnNum, columnIndex) {
+let dropDisc = function (boardModel, columnIndex) {
   let black = 1
   let red = 2
   let currentDisc = black
-  let columnArray = columnNum[columnIndex]
+  let discClass= "playerBlack"
   let player = turnTracker(numberOfDiscsPlayed)
-  
-  if (columnFull(columnArray) === true || gameWon(columnNum) === true) {
+
+  if (columnFull(boardModel, columnIndex) === true || gameWon(boardModel) === true) {
     return
   } else {
     if (player === "red") {
       currentDisc = red
+      discClass = "playerRed"
     }
-    for (let i = 6; i >= 0; i -= 1) {
-      if (columnArray[i] === 0) {
-        columnArray[i] = currentDisc
+    for (let rowIndex = 5; rowIndex >= 0; rowIndex -= 1) {
+      if (boardModel[rowIndex][columnIndex] === 0) {
+        boardModel[rowIndex][columnIndex] = currentDisc
         numberOfDiscsPlayed += 1
         console.log(board)
-      }  for (let j = 0; j < columnArray.length + 1; j += 1) {
-        let gameSlot= document.getElementsByClassName("emptySpace") 
-              let columnSlot = columnArray[j]
-              if ((columnSlot = 1)) {
-                gameSlot.classList = "playerBlack"
-              } else if ((columnSlot = 2)) {
-                gameSlot.classList = "playerRed"
-              }               
-              //gameSlot.append()
-            }
-          }
+        console.log(rowIndex)
+        let columnArray= document.querySelectorAll (`#column${columnIndex} .emptySpace`)
+        let gameSlot= columnArray[rowIndex]
+        gameSlot.classList.add(discClass)
+        gameSlot.classList.remove("emptySpace")
+        console.log(columnArray)
+        return
       }
-      setUpClickHandlers()
-      }
-    
-    
-  
+    }
+  }
+}
+
+
+
 
 
 let displayMessage = function (message) {
- messageBox.innerHTML = message
+  messageBox.innerHTML = message
 };
 
 console.table(board)
@@ -94,12 +92,12 @@ let whoIsTheWinner = function (boardModel) {
   //return the player number who won, or null
 }
 
-let gameWon= function(winner, tieGame) {}
+let gameWon = function (winner, tieGame) { }
 
 let isGameATie = function () {
-    if (numberOfDiscsPlayed === 42){
-      return true
-    }
+  if (numberOfDiscsPlayed === 42) {
+    return true
+  }
   return false
 }
 
@@ -112,16 +110,16 @@ let turnTracker = function (turnNumber) {
 }
 
 let nextPlayer = function () {
-  if (currentPlayer === 1){
+  if (currentPlayer === 1) {
     currentPlayer = 2
     displayMessage(`Player ${currentPlayer} it's your turn`)
-  }else if (currentPlayer === 2){
+  } else if (currentPlayer === 2) {
     currentPlayer = 1
     displayMessage(`Player ${currentPlayer} it's your turn`)
   }
 }
 
-let gameOver = function() {
+let gameOver = function () {
 
 }
 
@@ -137,40 +135,40 @@ let checkForGameOver = function (boardModel) {
 };
 
 let setUpClickHandlers = function () {
-let col1 = document.querySelector("#column1")
-col1.addEventListener("click", function(){
-  dropDisc(board, 0)
-})
+  let col1 = document.querySelector("#column0")
+  col1.addEventListener("click", function () {
+    dropDisc(board, 0)
+  })
 
-let col2 = document.querySelector("#column2")
-col2.addEventListener("click", function(){
-  dropDisc(board, 1)
-})
+  let col2 = document.querySelector("#column1")
+  col2.addEventListener("click", function () {
+    dropDisc(board, 1)
+  })
 
-let col3 = document.querySelector("#column3")
-col3.addEventListener("click", function(){
-  dropDisc(board, 2)
-})
+  let col3 = document.querySelector("#column2")
+  col3.addEventListener("click", function () {
+    dropDisc(board, 2)
+  })
 
-let col4 = document.querySelector("#column4")
-col4.addEventListener("click", function(){
-  dropDisc(board, 3)
-})
+  let col4 = document.querySelector("#column3")
+  col4.addEventListener("click", function () {
+    dropDisc(board, 3)
+  })
 
-let col5 = document.querySelector("#column5")
-col5.addEventListener("click", function(){
-  dropDisc(board, 4)
-})
+  let col5 = document.querySelector("#column4")
+  col5.addEventListener("click", function () {
+    dropDisc(board, 4)
+  })
 
-let col6 = document.querySelector("#column6")
-col6.addEventListener("click", function(){
-  dropDisc(board, 5)
-})
+  let col6 = document.querySelector("#column5")
+  col6.addEventListener("click", function () {
+    dropDisc(board, 5)
+  })
 
-let col7 = document.querySelector("#column7")
-col7.addEventListener("click", function(){
-  dropDisc(board, 6)
-})
+  let col7 = document.querySelector("#column6")
+  col7.addEventListener("click", function () {
+    dropDisc(board, 6)
+  })
 }
 
 
@@ -214,4 +212,14 @@ let testBoard3 = [
   [2, 0, 0, 0, 0, 0, 0],
   [2, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
+]
+
+
+let testBoard4 = [
+  [1, 0, 0, 0, 0, 0, 0],
+  [2, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
+  [2, 0, 0, 0, 0, 0, 0],
+  [2, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
 ]
